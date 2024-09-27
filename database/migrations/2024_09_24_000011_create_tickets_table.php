e722e14bb55e77713635cec28d2ff60a84f1c703
DongTimor <dongnk.961@gmail.com>
2024-09-27 09:21:21 +0700

    feature/migration : Rename tables

--- database/migrations/2024_09_24_000011_create_tickets_table.php
+++ database/migrations/2024_09_24_000011_create_tickets_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tikets', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('seat_id')->constrained('seats')->onDelete('cascade');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tikets');
+        Schema::dropIfExists('tickets');
     }
 };
