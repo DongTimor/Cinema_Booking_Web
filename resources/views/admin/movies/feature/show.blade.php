ebb7995ba82b576cf0bf75fecfc89806d4426904
ViT <vinhtoan552@gmail.com>
2024-10-11 11:59:16 +0700

    feature/movie: remove blank space,comment

--- resources/views/admin/movies/feature/show.blade.php
+++ resources/views/admin/movies/feature/show.blade.php
@@ -8,7 +8,6 @@
 @section('content')
     <div class="">
         <h1>Movie Details</h1>
-
         <div style="display: flex; flex-direction: row; gap: 30px;">
             <div class="w-100">
                 <strong>Name:</strong>
@@ -37,8 +36,6 @@
                 </ul>
             </div>
         </div>
-
-        <!-- Display Images -->
         <div class="form-group">
             <strong>Images:</strong>
             <div class="d-flex flex-wrap">
@@ -49,17 +46,14 @@
                 @endforeach
             </div>
         </div>
-
         <div class="form-group">
             <strong>Trailer:</strong>
             <p>{{ $movie->trailer }}</p>
         </div>
-
         <div class="form-group">
             <strong>Description:</strong>
             <p>{{ $movie->description }}</p>
         </div>
-
         <div class="mt-3">
             <a href="{{ route('movies.features.index') }}" class="btn btn-primary">Back to Movies</a>
         </div>
