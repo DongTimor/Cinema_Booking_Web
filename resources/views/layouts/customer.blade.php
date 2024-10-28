51b7827345170346618aef55bd1be5269a02840e
DaoPTA <phantaanhdao@gmail.com>
2024-10-28 17:38:09 +0700

    feature/customer: fix bug register customer

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="alternate icon" class="js-site-favicon" type="*/x-icon" href="{{ asset('favicon.ico') }}">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
