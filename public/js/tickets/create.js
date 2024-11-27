2ca674f4fd4f5772c51c0288cc13db405fc0e4e2
tcquan-dev <tcquan.it@gmail.com>
2024-11-27 15:59:43 +0700

    review/tickets: clear seats when change showtime

--- public/js/tickets/create.js
+++ public/js/tickets/create.js
@@ -79,6 +79,7 @@ $("#showtime").on("change", async function () {
     if (this.value) {
         const movie = $("#movie").val();
         const auditoriums = await fetchAuditoriums(movie, date, this.value);
+        $("#seats").html("");
         $("#auditorium").html(auditoriums);
     }
 });
